b5d69d0350a1922dfc58423822b35bcce12c44c2
Totti <totti.prtama@gmail.com>
2024-12-28 22:03:42 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,12 +15,11 @@ Route::get('/user', function (Request $request) {
 Route::get('dokter', [DokterController::class, 'getDokter']);
 Route::get('dokter/{id}', [DokterController::class, 'getDokterById']);
 Route::post('dokter', [DokterController::class, 'storeDokter']);
-
 Route::delete('dokter/{id}', [DokterController::class, 'destroyDokter']);
 Route::put('dokter/{id}', [DokterController::class, 'updateDokter']);
 
 Route::get('pasien', [PasienController::class, 'getPasien']);
-Route::get('pasien/{id}', [DokterController::class, 'getPasienById']);
+Route::get('pasien/{id}', [PasienController::class, 'getPasienById']);
 Route::post('pasien', [PasienController::class, 'storePasien']);
 Route::delete('pasien/{id}', [PasienController::class, 'destroyPasien']);
 Route::put('pasien/{id}', [PasienController::class, 'updatePasien']);
@@ -32,13 +31,13 @@ Route::delete('obat/{id}', [ObatController::class, 'destroyObat']);
 Route::put('obat/{id}', [ObatController::class, 'updateObat']);
 
 Route::get('kunjungan', [KunjunganController::class, 'getKunjungan']);
-Route::get('kunjungan/{id}', [DokterController::class, 'getKunjunganById']);
+Route::get('kunjungan/{id}', [KunjunganController::class, 'getKunjunganById']);
 Route::post('kunjungan', [KunjunganController::class, 'storeKunjungan']);
 Route::delete('kunjungan/{id}', [KunjunganController::class, 'destroyKunjungan']);
 Route::put('kunjungan/{id}', [KunjunganController::class, 'updateKunjungan']);
 
 Route::get('rekamMedis', [RekamMedisController::class, 'getRekamMedis']);
-Route::get('rekamMedis/{id}', [DokterController::class, 'getRekamMedisById']);
+Route::get('rekamMedis/{id}', [RekamMedisController::class, 'getRekamMedisById']);
 Route::post('rekamMedis', [RekamMedisController::class, 'storeRekamMedis']);
 Route::delete('rekamMedis/{id}', [RekamMedisController::class, 'destroyRekamMedis']);
 Route::put('rekamMedis/{id}', [RekamMedisController::class, 'updateRekamMedis']);
